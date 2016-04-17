cf087b6ec502644afc2988b94df217644c07723c
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-04-17 15:05:29 +0300

    Added tests - removed min stability

---
+++ tests/TreeTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace samsonframework\html2less\tests;
+
+/**
+ * Created by <egorov@samsonos.com>
+ * on 18.04.16 at 17:20
+ */
+class TreeTest extends \PHPUnit_Framework_TestCase
+{
+    /** Tests init */
+    public function setUp()
+    {
+
+    }
+
+    public function testCount()
+    {
+        $this->asse
+	}
+}
